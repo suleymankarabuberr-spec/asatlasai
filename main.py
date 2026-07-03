c35edf052fdbd9784ce2932cba4b30c1ba2a40fa
suleymankarabuberr-spec <suleymankarabuberr@gmail.com>
2026-07-03 21:47:47 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
@@ -12,26 +13,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/")
-async def home():
-    return FileResponse("templates/index.html")
-
-
-@app.post("/chat")
-async def chat():
-    return {
-        "response": "Merhaba! Ben asATLASAI 🚀"
-    }
-from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
