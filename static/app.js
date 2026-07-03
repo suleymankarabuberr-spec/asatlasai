327edb075207259c429da4c2b0c435b45578ed20
suleymankarabuberr-spec <suleymankarabuberr@gmail.com>
2026-07-03 21:42:04 +0300

    app.js

--- static/app.js
+++ static/app.js
@@ -19,3 +19,26 @@ document.getElementById("messages").innerHTML+=
 "<p><b>asATLASAI:</b> "+data.response+"</p>";
 
 }
+async function send() {
+    const input = document.getElementById("message");
+    const messages = document.getElementById("messages");
+
+    const text = input.value;
+    if (!text) return;
+
+    messages.innerHTML += `<div><b>Sen:</b> ${text}</div>`;
+
+    const res = await fetch("/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message: text })
+    });
+
+    const data = await res.json();
+
+    messages.innerHTML += `<div><b>asATLASAI:</b> ${data.response}</div>`;
+
+    input.value = "";
+}
