ee5cf1b2725794ff794cde5295b599d2795b9c8c
suleymankarabuberr-spec <suleymankarabuberr@gmail.com>
2026-07-03 21:42:41 +0300

    main.py

--- main.py
+++ main.py
@@ -26,3 +26,27 @@ async def chat():
     return {
         "response": "Merhaba! Ben asATLASAI 🚀"
     }
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+class ChatRequest(BaseModel):
+    message: str
+
+
+@app.get("/")
+async def home():
+    return FileResponse("templates/index.html")
+
+
+@app.post("/chat")
+async def chat(req: ChatRequest):
+    return {
+        "response": f"Şunu söyledin: {req.message}"
+    }
